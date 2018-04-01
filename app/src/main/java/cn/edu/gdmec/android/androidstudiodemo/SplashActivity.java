390ecc9eca536f967f83f8f89b7bce70a32f0ef1
huangguangda <2397923107@qq.com>
2018-04-01 23:13:12 +0800

    12

--- app/src/main/java/cn/edu/gdmec/android/androidstudiodemo/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/androidstudiodemo/SplashActivity.java
@@ -1,8 +1,17 @@
 package cn.edu.gdmec.android.androidstudiodemo;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Jack on 2018/4/1.
@@ -20,5 +29,27 @@ public class SplashActivity extends AppCompatActivity{
     }
 
     private void init() {
+        TextView tv_version = findViewById(R.id.tv_version);
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(),0);
+            tv_version.setText("version:"+packageInfo.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            tv_version.setText("version");
+        }
+        //利用timer让此界面延迟3秒后跳转，timer有一个线程，该线程不断执行task
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                //发送intent实现页面跳转，第一个参数为当前页面的context，第二个参数为要跳转的主页
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                //跳转后关闭当前欢迎页面
+                SplashActivity.this.finish();
+            }
+        };
+        //调度执行timerTask，第二个参数传入延迟时间（毫秒）
+        timer.schedule(timerTask,3000);
     }
 }
