aaebe7716ea53f5649c437b780b98ef541615d75
huangguangda <2397923107@qq.com>
2018-04-01 23:00:17 +0800

    12

--- app/src/main/java/cn/edu/gdmec/android/androidstudiodemo/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/androidstudiodemo/SplashActivity.java
@@ -1,8 +1,24 @@
 package cn.edu.gdmec.android.androidstudiodemo;
 
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Jack on 2018/4/1.
  */
 
-public class SplashActivity {
+public class SplashActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //设置此界面为
+        // 竖屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        init();
+    }
+
+    private void init() {
+    }
 }
